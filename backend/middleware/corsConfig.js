2b02d85351de9f9023e7c851770179befc85df56
Aryansingh009 <153926901+Aryansingh009@users.noreply.github.com>
2026-04-07 15:07:05 +0530

    db

--- backend/middleware/corsConfig.js
+++ backend/middleware/corsConfig.js
@@ -1,6 +1,18 @@
-const FRONTEND_URL = process.env.FRONTEND_URL || process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://rentify11-aryan-s-projects-323c3d5d.vercel.app",
+  "https://rentify11.vercel.app",
+];
 
 export const corsOptions = {
-  origin: FRONTEND_URL,
+  origin: function (origin, callback) {
+    console.log("Origin:", origin); // debug
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(null, false); // ✅ FIXED
+    }
+  },
   credentials: true,
 };
